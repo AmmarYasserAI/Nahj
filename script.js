73040917a55dc3acf7a7310395da9bfb57281fad
AmmarYasserAI <165922566+AmmarYasserAI@users.noreply.github.com>
2024-08-28 21:43:37 +0300

    Hamburger icons

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
+const navItems = document.querySelectorAll('.nav a');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   nav.classList.toggle('active');
+});
+
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    nav.classList.remove('active');
+    hamburger.classList.remove('active'); // Add this line
+  });
 });
